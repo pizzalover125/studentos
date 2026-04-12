49e6dbae7050c3a630be53d27e5d68ecc8c0a6e3
Aaradhya Panda <chessthinker600@gmail.com>
2026-04-12 10:11:40 -0700

    test: update system test setup to sign_in before each test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,7 +4,14 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 900]
 
   setup do
-    visit root_path
+    sign_in
     page.execute_script("localStorage.clear()")
+  end
+
+  private
+
+  def sign_in
+    visit "/test/sign_in"
+    assert_current_path root_path
   end
 end
